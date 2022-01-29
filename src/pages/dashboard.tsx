753b66b16db9cc4069b832b5fb3914859e6a8d4f
Thiago Spart <user000.000resu@gmail.com>
2022-01-29 18:43:40 -0300

    Add sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,17 @@
+import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
 
 import { Header } from "../components/header";
+import { Sidebar } from "../components/sidebar";
 
 const DashBoard: NextPage = () => {
   return (
-    <div>
+    <Flex as="main" direction="column" h="100vh">
       <Header />
-      <p>oi</p>
-    </div>
+      <Flex w="100%" my="6" maxW="1480" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 };
 
