daeed7c7e4601e48f07090eccad1bea99e182701
Thiago Spart <user000.000resu@gmail.com>
2022-02-07 13:11:22 -0300

    Pagination componentization

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+import { PaginationItem } from "./paginationItem";
+
+export const Pagination = () => {
+  return (
+    <Stack
+      direction="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> of <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+      </Stack>
+    </Stack>
+  );
+};
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,7 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import { Pagination } from "../../components/pagination";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 const UserList = () => {
