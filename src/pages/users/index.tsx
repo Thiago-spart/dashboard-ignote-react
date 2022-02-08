1cc6c74c74390150636fd8e4c0e6cf577ed64e61
Thiago Spart <user000.000resu@gmail.com>
2022-02-08 13:53:01 -0300

    Add responsive on users table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -19,6 +20,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 const UserList = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -44,17 +50,17 @@ const UserList = () => {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" w="32">
+                <Th px={["4", "4", "6"]} color="gray.300" w="32">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Users</Th>
-                <Th>Registration Date</Th>
-                <Th w="8"></Th>
+                {isWideVersion && <Th>Registration Date</Th>}
+                {isWideVersion && <Th w="8"></Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -65,18 +71,20 @@ const UserList = () => {
                     </Text>
                   </Box>
                 </Td>
-                <Td>02 February, 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Edit
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>02 February, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Edit
+                    </Button>
+                  </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
