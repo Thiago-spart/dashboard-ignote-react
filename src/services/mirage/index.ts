72027a1c8f7c02313ab46f9ad9efaf521a4f2bb8
Thiago Spart <user000.000resu@gmail.com>
2022-02-24 16:53:25 -0300

    Add pagination on mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs"
+import { createServer, Factory, Model, Response } from "miragejs"
 
 import faker from "faker"
 
@@ -27,13 +27,26 @@ export const makeServer = () => {
 			})
 		},
 		seeds(server) {
-			server.createList("user", 10)
+			server.createList("user", 200)
 		},
 		routes() {
 			this.namespace = "api"
 			this.timing = 750;
 
-			this.get("/users")
+			this.get("/users", (schema, req) => {
+				const { page = 1, per_page = 10 } = req.queryParams
+
+				const total = schema.all("user").length
+				
+				const pageStart = (Number(page) - 1) *  Number(per_page)
+				const pageEnd = pageStart + Number(per_page)
+
+				const users = this.serialize(schema.all("user"))
+					.users.slice(pageStart, pageEnd)
+			
+				return new Response(200, {"x-total-count": String(total)}, { users})
+			})
+
 			this.post("/users")
 
 			this.namespace = ""
